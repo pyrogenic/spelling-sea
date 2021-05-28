92b367596c653653047734e9d837f84a8339ae62
Joshua Pollak <abottomlesspit@gmail.com>
2021-05-28 13:44:28 -0700

    Updates

---
+++ precache-manifest.05ad216781894b22c7430784f42733fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1299805b40814b196a0bae65872840",
+    "url": "/spelling-sea/index.html"
+  },
+  {
+    "revision": "1bc2778a3e1048f60d8e",
+    "url": "/spelling-sea/static/css/main.6633c8a4.chunk.css"
+  },
+  {
+    "revision": "24eacacc2006bc0575cc",
+    "url": "/spelling-sea/static/js/2.b4977b98.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/spelling-sea/static/js/2.b4977b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc2778a3e1048f60d8e",
+    "url": "/spelling-sea/static/js/main.c31b9a07.chunk.js"
+  },
+  {
+    "revision": "0a36257cc9b80bcfad00",
+    "url": "/spelling-sea/static/js/runtime-main.95d48bc1.js"
+  },
+  {
+    "revision": "1ae50cddaa7e4c367acc35429c726592",
+    "url": "/spelling-sea/static/media/isle.1ae50cdd.jpg"
+  }
+]);
